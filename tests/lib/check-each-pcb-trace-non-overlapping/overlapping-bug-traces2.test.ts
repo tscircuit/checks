63e4fd3ddb2eb8c2749fb25c9ccf84be319a0930
seveibar <seveibar@gmail.com>
2024-07-24 22:39:50 -0700

    reproduction for bad traces error (traces do not overlap)

--- tests/lib/check-each-pcb-trace-non-overlapping/overlapping-bug-traces2.test.ts
+++ tests/lib/check-each-pcb-trace-non-overlapping/overlapping-bug-traces2.test.ts
@@ -4,7 +4,7 @@ import type { AnySoupElement, PCBTrace, PCBSMTPad } from "@tscircuit/soup"
 import traces2 from "tests/assets/traces2.solution.json"
 
 describe("checkEachPcbTraceNonOverlapping", () => {
-  test("should return no errors when traces don't overlap", () => {
+  test("traces2 should have one trace overlap error", () => {
     const soup: AnySoupElement[] = traces2 as any
 
     expect(checkEachPcbTraceNonOverlapping(soup)).toHaveLength(1)
---
+++ tests/lib/check-each-pcb-trace-non-overlapping/overlapping-bug-traces3.test.ts
@@ -0,0 +1,13 @@
+import { expect, test, describe } from "bun:test"
+import { checkEachPcbTraceNonOverlapping } from "lib/check-each-pcb-trace-non-overlapping"
+import type { AnySoupElement, PCBTrace, PCBSMTPad } from "@tscircuit/soup"
+import traces3 from "tests/assets/traces3.solution.json"
+
+describe("checkEachPcbTraceNonOverlapping", () => {
+  test("should return no errors when traces don't overlap", () => {
+    const soup: AnySoupElement[] = traces3 as any
+
+    console.log(checkEachPcbTraceNonOverlapping(soup))
+    expect(checkEachPcbTraceNonOverlapping(soup)).toHaveLength(0)
+  })
+})
