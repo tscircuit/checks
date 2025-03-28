cee4490d32ad80ee79eb639389c5059ec414b446
seveibar <seveibar@gmail.com>
2025-03-27 18:55:32 -0700

    update snapshot

--- tests/lib/check-each-pcb-trace-non-overlapping/keyboard1.test.ts
+++ tests/lib/check-each-pcb-trace-non-overlapping/keyboard1.test.ts
@@ -6,5 +6,5 @@ test("keyboard1", () => {
   const errors = checkEachPcbTraceNonOverlapping(keyboard1)
 
   // Placeholder for snapshot sake, have no idea what this number should be
-  expect(errors.length).toBe(85)
+  expect(errors.length).toBe(139)
 })
