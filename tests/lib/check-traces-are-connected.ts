c1fa0d922d35b492551a1fef7c8467ec630d5ebf
Shibo <shibosoftwaredev@gmail.com>
2025-04-20 23:28:27 +0200

    rename

--- tests/lib/check-traces-are-connected.ts
+++ tests/lib/check-traces-are-connected.ts
@@ -3,12 +3,12 @@ import { checkTracesAreConnected } from "lib/check-traces-are-connected"
 import soup from "tests/assets/traces1.solution.json"
 import soup2 from "tests/assets/traces5-missing-trace-end.json"
 
-describe("checkEachPcbPortConnected", () => {
-  test("should not error as traces are contiguous", () => {
+describe("testing checkTracesAreConnected(", () => {
+  test("should not error as traces are connected", () => {
     const errors = checkTracesAreConnected(soup as any)
     expect(errors).toHaveLength(0)
   })
-  test("should error as traces are not contiguous", () => {
+  test("should error as a trace is not fully connected", () => {
     const errors = checkTracesAreConnected(soup2 as any)
     expect(errors).not.toHaveLength(0)
   })
