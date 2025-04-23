39bcb01c2bed68ba154e721ca44458ad2d272aa8
Your Name <you@example.com>
2025-04-23 08:01:42 +0200

    improved

--- tests/lib/check-component-out-of-board.test.ts
+++ tests/lib/check-component-out-of-board.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "bun:test"
 import componentOutOfBoard from "tests/assets/component-out-of-board.json"
-import { checkPcbComponentsOutOfBoard } from "lib/check-pcb-components-outside-board/checkPcbComponentsOutOfBoard"
+import { checkPcbComponentsOutOfBoard } from "lib/check-pcb-components-out-of-board/checkPcbComponentsOutOfBoard"
 test("pcb component out of board", async () => {
   const pcbPlacementError = checkPcbComponentsOutOfBoard(
     componentOutOfBoard as any,
