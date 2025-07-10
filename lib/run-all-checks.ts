95f676be9c7877a4292b886fa82cbc294a269782
seveibar <seveibar@gmail.com>
2025-07-10 07:55:30 -0700

    fix major issue breaking eval (import of tscircuit)

--- lib/run-all-checks.ts
+++ lib/run-all-checks.ts
@@ -1,4 +1,3 @@
-import { runTscircuitCode } from "tscircuit"
 import { checkEachPcbPortConnected } from "./check-each-pcb-port-connected"
 import { checkEachPcbTraceNonOverlapping } from "./check-each-pcb-trace-non-overlapping/check-each-pcb-trace-non-overlapping"
 import { checkSameNetViaSpacing } from "./check-same-net-via-spacing"
@@ -6,14 +5,13 @@ import { checkViasOffBoard } from "./check-pcb-components-out-of-board/checkVias
 import { checkPcbComponentsOutOfBoard } from "./check-pcb-components-out-of-board/checkPcbComponentsOutOfBoard"
 import { checkTracesAreContiguous } from "./check-traces-are-contiguous/check-traces-are-contiguous"
 
-export async function runAllChecks(code: string) {
-  const circuitJson = await runTscircuitCode(code)
+export async function runAllChecks(circuitJson: any[]) {
   return [
-    ...checkEachPcbPortConnected(circuitJson as any),
-    ...checkEachPcbTraceNonOverlapping(circuitJson as any),
-    ...checkSameNetViaSpacing(circuitJson as any),
-    ...checkViasOffBoard(circuitJson as any),
-    ...checkPcbComponentsOutOfBoard(circuitJson as any),
-    ...checkTracesAreContiguous(circuitJson as any),
+    ...checkEachPcbPortConnected(circuitJson),
+    ...checkEachPcbTraceNonOverlapping(circuitJson),
+    ...checkSameNetViaSpacing(circuitJson),
+    ...checkViasOffBoard(circuitJson),
+    ...checkPcbComponentsOutOfBoard(circuitJson),
+    ...checkTracesAreContiguous(circuitJson),
   ]
 }
