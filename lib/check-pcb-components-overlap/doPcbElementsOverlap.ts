ed68714db8412b0686b5d329a10cfdf43eeaf4e6
rushabhcodes <mail@rushabh.dev>
2026-03-13 00:44:46 +0530

    format

--- lib/check-pcb-components-overlap/doPcbElementsOverlap.ts
+++ lib/check-pcb-components-overlap/doPcbElementsOverlap.ts
@@ -1,18 +1,16 @@
 import { getBoundsOfPcbElements } from "@tscircuit/circuit-json-util"
 import { doBoundsOverlap } from "@tscircuit/math-utils"
 import type { PcbHole, PcbPlatedHole, PcbSmtPad } from "circuit-json"
+import type { Collidable } from "lib/check-each-pcb-trace-non-overlapping/getCollidableBounds"
 import { getLayersOfPcbElement } from "lib/util/getLayersOfPcbElement"
 
 export type OverlappableElement = PcbSmtPad | PcbPlatedHole | PcbHole
 
 function getElementLayers(elem: OverlappableElement): string[] {
-  return getLayersOfPcbElement(elem as any)
+  return getLayersOfPcbElement(elem as Collidable)
 }
 
-function doLayersOverlap(
-  layers1: string[],
-  layers2: string[],
-): boolean {
+function doLayersOverlap(layers1: string[], layers2: string[]): boolean {
   if (layers1.length === 0 || layers2.length === 0) return true
   return layers1.some((l) => layers2.includes(l))
 }
