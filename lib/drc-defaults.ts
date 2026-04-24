1d345b3e79b2a31edf3396a0d637fe65ebba814c
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2026-04-24 05:59:10 +0200

    types

--- lib/drc-defaults.ts
+++ lib/drc-defaults.ts
@@ -6,13 +6,13 @@ export const DEFAULT_VIA_DIAMETER = jlcMinTolerances.min_via_pad_diameter
 export const DEFAULT_VIA_BOARD_MARGIN = 0.3
 
 export const DEFAULT_SAME_NET_VIA_MARGIN =
-  jlcMinTolerances.min_via_to_via_clearance
+  jlcMinTolerances.min_via_hole_edge_to_via_hole_edge_clearance
 export const DEFAULT_DIFFERENT_NET_VIA_MARGIN =
-  jlcMinTolerances.min_via_to_via_clearance
+  jlcMinTolerances.min_via_hole_edge_to_via_hole_edge_clearance
 
 export const DEFAULT_PAD_PAD_CLEARANCE =
-  jlcMinTolerances.min_pad_to_pad_clearance
+  jlcMinTolerances.min_pad_edge_to_pad_edge_clearance
 export const DEFAULT_PAD_TRACE_CLEARANCE =
-  jlcMinTolerances.min_trace_to_pad_clearance
+  jlcMinTolerances.min_trace_to_pad_edge_clearance
 
 export const EPSILON = 0.005
