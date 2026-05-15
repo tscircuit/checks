1c7ae31c9e9bb5fb073d236c9d58e05253b09d00
mohan-bee <mohn08052006@gmail.com>
2026-05-15 13:36:01 +0530

    up

--- lib/check-pcb-components-overlap/checkPcbComponentOverlap.ts
+++ lib/check-pcb-components-overlap/checkPcbComponentOverlap.ts
@@ -131,17 +131,6 @@ export function checkPcbComponentOverlap(
     componentMap.get(componentId)!.elements.push(courtyard)
   }
 
-  for (const courtyard of courtyards) {
-    const componentId = courtyard.pcb_component_id
-    if (!componentMap.has(componentId)) {
-      componentMap.set(componentId, {
-        component_id: componentId,
-        elements: [],
-      })
-    }
-    componentMap.get(componentId)!.elements.push(courtyard)
-  }
-
   // Compute bounds for each component
   for (const [componentId, componentData] of componentMap) {
     if (componentData.elements.length > 0) {
